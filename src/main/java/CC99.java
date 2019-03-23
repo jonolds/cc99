1fbfaae2f31f660ca165886ae5d4737ff77c1b05
jonolds <jonolds@gmail.com>
2019-03-23 11:35:28 -0500

    win

--- src/main/java/CC99.java
+++ src/main/java/CC99.java
@@ -1,17 +1,8 @@
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.WildcardFileFilter;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -28,28 +19,21 @@ import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-/** Input is a map in adjacency list format, and performs a breadth-first search. The input format is:
- * "ID	 EDGES|DISTANCE|COLOR"
- * 		ID = the unique identifier for a node (assumed to be an int here)
- * 		EDGES = the list of edges emanating from the node (e.g. 3,8,9,12)
- * 		DISTANCE = the to be determined distance of the node from the source
- * 		COLOR = a simple status tracking field to keep track of when we're finished with a node
- * Source should have distance 0 and be GRAY. Others have distance Integer.MAX_VALUE and color WHITE.*/
-public class CC99 extends Configured implements Tool {
-	int iters = 6;
-	/** WHITE or BLACK nodes are emitted as is. For every edge of a Color.GRAY node, we emit a new Node
-	 * with distance incremented by one. The Color.GRAY node is then colored black and is also emitted. */
+public class CC99 extends MRHelp implements Tool {
+	String log = ""; int maxIters = 8, mp = 3, rd = 3;
+	/** WHITE and BLACK nodes are emitted as is. For every edge of a GRAY node, we emit a new Node with 
+	 * distance incremented by one. The Color.GRAY node is then colored black and is also emitted. */
 	public static class MapClass extends MapReduceBase implements Mapper<LongWritable, Text, IntWritable, Text> {
 		public void map(LongWritable key, Text value, OutputCollector<IntWritable, Text> output, Reporter reporter) throws IOException {
 			Node node = new Node(value.toString());
 
 			// For each GRAY node, emit each of the edges as a new node (also GRAY)
 			if (node.getColor() == Color.GRAY) {
-				for (int edge : node.getEdges()) {
-					Node vnode = new Node(edge);
-					vnode.setCost(node.getCost() + 1);
-					vnode.setColor(Color.GRAY);
-					output.collect(new IntWritable(vnode.getId()), vnode.getLine());
+				for(int i = 0; i < node.getEdges().size(); i++) {
+					Node dummynode = new Node(node.getEdges().get(i));
+					dummynode.setCost(node.getWeights().get(i) + node.getCost());
+					dummynode.setColor(Color.GRAY);
+					output.collect(new IntWritable(dummynode.getId()), dummynode.getLine());
 				}
 				node.setColor(Color.BLACK);
 			}
@@ -59,32 +43,32 @@ public class CC99 extends Configured implements Tool {
 
 	/** A reducer class that just emits the sum of the input values. */
 	public static class Reduce extends MapReduceBase implements Reducer<IntWritable, Text, IntWritable, Text> {
+
 		/** Make a new node which combines all information for this single node id. The Node should have
 		 * - 1)The full list of edges. 2)The minimum distance. 3)The darkest Color. */
 		public void reduce(IntWritable key, Iterator<Text> values, OutputCollector<IntWritable, Text> output, Reporter reporter) throws IOException {
 			List<String> vals = itToList(values);
 			
-			List<Integer> edges = null;
-			int distance = Integer.MAX_VALUE;
+			List<Integer> edges = null, weights = null;
+			int cost = Integer.MAX_VALUE;
 			Color color = Color.WHITE;
-			
-			println(key + "  " + vals.stream().map(x->x).collect(Collectors.joining("\t\t")));
-			for(String value: vals) {
+
+			print(key);
+			for(String value : vals) {
+				println("\t" + value);
 				Node u = new Node(key.get() + "\t" + value);
-				// One one copy of the node will be the fully expanded version, which includes the edges
+
 				if(u.getEdges().size() > 0)
 					edges = u.getEdges();
-				// Save the minimum distance/darkest color
-				if(u.getCost() < distance)
-					distance = u.getCost();
-				if(u.getColor().ordinal() > color.ordinal())
-					color = u.getColor();
+				if(u.getWeights().size() > 0)
+					weights = u.getWeights();
+				// Save the minimum cost and darkest color
+				if(u.getCost() < cost)
+					cost = u.getCost();
+				color = u.getColor();
+//				color = (u.getColor().ordinal() > color.ordinal()) ? u.getColor() : color;
 			}
-
-			Node n = new Node(key.get());
-			n.setCost(distance);
-			n.setEdges(edges);
-			n.setColor(color);
+			Node n = new Node(key.get(), edges, weights, cost, color);
 			output.collect(key, new Text(n.getLine()));
 		}
 	}
@@ -92,63 +76,49 @@ public class CC99 extends Configured implements Tool {
 	/** The main driver for word count map/reduce program. Invoke this method to submit the map/reduce job.
 	     @throws IOException When there is communication problems with the job tracker. */
 	public int run(String[] args) throws Exception {
-		int iterCount = 0;
-		while (iterCount < iters) {
-			JobConf conf = getJobConf(args);
-			String input = (iterCount == 0) ? "input" : "output-graph-" + iterCount;
+		//Get command line arguments. -i <#Iterations>  is required.
+//		int maxIters = 11, mapNum = 3, redNum = 3;
+		int mapNum = 3, redNum = 3;
+
+		for (int i = 0; i < args.length; ++i) {
+			mapNum = ("-m".equals(args[i])) ? Integer.parseInt(args[++i]) : mapNum;
+			redNum = ("-r".equals(args[i])) ? Integer.parseInt(args[++i]) : redNum;
+//			maxIters = ("-i".equals(args[i])) ? Integer.parseInt(args[++i]) : maxIters;
+		}
+		if (maxIters < 1) {
+			System.err.println("Usage: -i <# of iterations> is a required command line argument");
+			System.exit(2);
+		}
+
+		for(int iters = 0; iters < maxIters; iters++) {
+			println("==========" + iters + "============");
+			JobConf conf = getJobConf(args, mp, rd);
+			String input = (iters == 0) ? "input" : "output-graph-" + iters;
 			FileInputFormat.setInputPaths(conf, new Path(input));
-			FileOutputFormat.setOutputPath(conf, new Path("output-graph-" + (iterCount + 1)));
+			FileOutputFormat.setOutputPath(conf, new Path("output-graph-" + (iters + 1)));
 			JobClient.runJob(conf);
-			iterCount++;
+			print("\n\n\n\n\n\n\n\n\n\n");
 		}
 		return 0;
 	}
 	
-	private JobConf getJobConf(String[] args) {
+	private JobConf getJobConf(String[] args, int mapNum, int redNum) {
 		JobConf conf = new JobConf(getConf(), CC99.class);
 		conf.setJobName("graphsearch");
 		conf.setOutputKeyClass(IntWritable.class);
 		conf.setOutputValueClass(Text.class);
 		conf.setMapperClass(MapClass.class);
 		conf.setReducerClass(Reduce.class);
-
-		for (int i = 0; i < args.length; ++i) {
-			if ("-m".equals(args[i]))
-				conf.setNumMapTasks(Integer.parseInt(args[++i]));
-			else if ("-r".equals(args[i]))
-				conf.setNumReduceTasks(Integer.parseInt(args[++i]));
-		}
+		conf.setNumMapTasks(mapNum);
+		conf.setNumReduceTasks(redNum);
 		return conf;
 	}
 	
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
-		deleteOutputFolders(conf);
+		clearOutput(conf);
 		int res = ToolRunner.run(conf, new CC99(), args);
-		combineOutputFolders(conf);
+		combineOutputs(conf, "output-graph");
 		System.exit(res);
-	}
-	
-	public static void deleteOutputFolders(Configuration conf) throws IllegalArgumentException, IOException {
-		List<String> folders = Files.list(Paths.get("")).map(x->x.toString()).filter(x->x.startsWith("output")).collect(Collectors.toList());
-		for(String folder : folders)
-			new Path(folder).getFileSystem(conf).delete(new Path(folder), true);
-	}	
-	public static void combineOutputFolders(Configuration conf) throws IOException {
-		Collection<File> test = FileUtils.listFiles(new File("."), new WildcardFileFilter("part*"), new WildcardFileFilter("*out*"));
-		FileUtils.writeStringToFile(new File("output" + File.separator + "outAll99.txt"), "input\n" + fileToStr("input/file0") + "\n", "UTF-8");
-		for(File x : test) {
-			FileUtils.writeStringToFile(new File("output" + File.separator + "outAll99.txt"), x.toString() + "\n" + fileToStr(x.toString()), "UTF-8", true);
-			FileUtils.deleteDirectory(new File(x.getParent()));
-		}
-	}
-	static <T>void println(T t) { System.out.println(t.toString()); }
-	public static String fileToStr(String filename) throws IOException {
-		return FileUtils.readFileToString(new File(filename), "UTF-8") + "\n";
-	}
-	public static List<String> itToList(Iterator<Text> it) {
-		List<String> vals = new ArrayList<>();
-		it.forEachRemaining(x->vals.add(x.toString()));
-		return vals;
 	}
 }
--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -6,10 +6,10 @@ import java.util.stream.Collectors;
 import org.apache.hadoop.io.Text;
 
 enum Color { WHITE, GRAY, BLACK }
-
-public class Node {
+//CC6 Node
+public class Node extends MRHelp {
 	private int id, cost;
-	private List<Integer> edges = new ArrayList<Integer>();
+	private List<Integer> edges = new ArrayList<>(), weights = new ArrayList<>();
 	private Color color = Color.WHITE;
 
 	public Node(String str) {
@@ -17,29 +17,44 @@ public class Node {
 		String key = map[0], value = map[1];
 		String[] tokens = value.split("\\|");
 
-		//ID - EDGES - COST - COLOR
+		//ID  -  EDGES  -  WEIGHTS  -  COST  -  COLOR
 		this.id = Integer.parseInt(key);
-		Arrays.stream(tokens[0].split(",")).filter(x->x.length()>0).forEach(x->edges.add(Integer.parseInt(x)));
-		this.cost = (tokens[1].equals("Integer.MAX_VALUE")) ? Integer.MAX_VALUE : Integer.parseInt(tokens[1]);
-		this.color = Color.valueOf(tokens[2]);
+		Arrays.stream(tokens[0].split(",")).map(x->x.trim()).filter(x->x.length()>0).forEach(x->edges.add(Integer.parseInt(x)));
+		Arrays.stream(tokens[1].split(",")).map(x->x.trim()).filter(x->x.length()>0).forEach(x->weights.add(Integer.parseInt(x)));
+		this.cost = (tokens[2].equals("MX")) ? Integer.MAX_VALUE : Integer.parseInt(tokens[2].trim());
+		this.color = Color.valueOf(tokens[3]);
 	}
-
+	public Node(int id, List<Integer> edges, List<Integer> weights, int cost, Color color) {
+		this.id = id; this.edges = edges; this.weights = weights; this.cost = cost; this.color = color;
+	}
 	public Node(int id) { this.id = id; }
-	public int getId() { return this.id; }
+		
+	public Text getLine() {
+		StringBuffer s = new StringBuffer();
+		
+		String edgeStr = edges.stream().map(x->x.toString()).collect(Collectors.joining(","));
+		s.append(post(edgeStr, 3) + "|");
+		
+		String weightStr = weights.stream().map(x->x.toString()).collect(Collectors.joining(","));
+		s.append(post(weightStr, 4)).append("|");
+		
+		String costStr = this.cost < Integer.MAX_VALUE ? Integer.valueOf(this.cost).toString() : "MX";
+		s.append(post(costStr, 2)).append("|");
+		s.append(color.toString());
+		return new Text(s.toString());
+	}	
 
+	public int getId() { return this.id; }
+	
 	public List<Integer> getEdges() { return this.edges; }
 	public void setEdges(List<Integer> edges) { this.edges = edges; }
 	
+	public List<Integer> getWeights() { return this.weights; }
+	public void setWeights(List<Integer> weights) { this.weights = weights; }
+	
 	public int getCost() { return this.cost; }
 	public void setCost(int cost) { this.cost = cost; }
-
+	
 	public Color getColor() { return this.color; }
 	public void setColor(Color color) { this.color = color; }
-
-	public Text getLine() {
-		StringBuffer s = new StringBuffer(edges.stream().map(x->x.toString()).collect(Collectors.joining(",")) + "|");
-		s.append(cost < Integer.MAX_VALUE ? cost : "Integer.MAX_VALUE").append("|");
-		s.append(color.toString());
-		return new Text(s.toString());
-	}
 }
